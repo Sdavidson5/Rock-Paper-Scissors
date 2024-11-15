7f438b7906e846435a9132c4e9a2c5419981419a
Sdavidson5 <86085106+Sdavidson5@users.noreply.github.com>
2024-11-14 22:07:17 -0500

    JS Work
    JS work

--- JS/test.js
+++ JS/test.js
@@ -1,6 +1,30 @@
-const nums = [;
+/*const array = [10,20,30];
 
-for (let i = 0; i < nums.length; i++) {
-  console.log(nums[i]);
+array[2] = 99;
+
+console.log(array)
+
+const array2 = ['hi','hello','good'];
+
+function getLastValue(){
+  return array2[array2.length-1]
 }
 
+console.log(getLastValue())
+
+const array3 = [1,20,22,24,5];
+
+console.log(array3)
+
+function arraySwap(){
+  [array3[0], array3[array3.length - 1]] = [array3[array3.length - 1], array3[0]]
+  return array3
+}
+
+console.log(arraySwap())
+*/
+
+
+for (let i = 5; i => 0; i -- ) {
+  console.log(i);
+}
